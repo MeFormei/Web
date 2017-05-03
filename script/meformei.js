2e081f03fe2dc6126b1386574f600988397c0462
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-05-02 21:36:45 -0300

    Ajuste no feedback de gesto para o usuario

--- script/meformei.js
+++ script/meformei.js
@@ -1,11 +1,14 @@
 const TOPIC_DIRECTION = 'direction';
 const TOPIC_POSITION = 'position';
+const TOPIC_STATUS = 'status';
 
 var client;
 var callbacks = {'WEST':null,'EAST':null,'NORTH':null,'SOUTH':null}
 var canvas = document.getElementById('feedback');
 var context = canvas.getContext("2d");
 
+const WIDTH_FACTOR = 0.3333; 
+const HEIGHT_FACTOR = 0.3333;
 
 
 function doConnection(ip, port){
@@ -20,30 +23,58 @@ function doConnection(ip, port){
 	client.on('message', function(topic, payload){
 		topicStr = topic.toString();
 		payloadStr = payload.toString();
+		
 		if(topicStr === TOPIC_DIRECTION){
+			
 			callbacks[payloadStr](); // calls callback function
-		} else if (topicStr === TOPIC_POSITION) {
+
+		} 
+		else if (topicStr === TOPIC_POSITION) {
+			
+			if(payloadStr == 'none'){
+				toggleFeedbackPanel(false);
+				return;
+			}
+
+			toggleFeedbackPanel(true);
 			coord = JSON.parse(payloadStr);
 			onCoordinates(coord.x, coord.y);
-			console.log(JSON.parse(payloadStr));
+			console.log(coord.x +" - "+ coord.y);
+
 		}
 	});
 }
 
 
 function onCoordinates(coordX, coordY) {
-	var relativeX = coordX / canvas.width; 
-	var relativeY = coordY / canvas.height;
-	var radius = 70;
+	var relativeX = coordX * WIDTH_FACTOR;
+	var relativeY = coordY * HEIGHT_FACTOR;
 
- 	context.beginPath();
-	context.arc(relativeX, relativeY, radius, 0, 2 * Math.PI, false);
-	context.fillStyle = 'green';
-	context.fill();
-	context.lineWidth = 5;
-	context.strokeStyle = '#003300';
-	context.stroke();	
+	context.clearRect(0, 0, canvas.width, canvas.height);
+ 	drawCircle(relativeX.toPrecision(2), relativeY.toPrecision(2));
 }
+
+
+function drawCircle(relativeX, relativeY) {
+
+	context.beginPath();
+	context.arc(relativeX, relativeY, 20, 0, 2 * Math.PI, true);
+	context.fillStyle =  "green";
+	context.fill();
+	context.strokeStyle = '#003300';
+	context.stroke();
+
+	requestAnimationFrame(drawCircle);
+}
+
+function toggleFeedbackPanel(flag) {
+	if (flag == false)
+		$(".feedback-movement").fadeOut(300);
+		
+	if (flag == true)
+		$(".feedback-movement").fadeIn(300);
+}
+
 
 function onLeft(callback){
 	callbacks['WEST'] = callback;
