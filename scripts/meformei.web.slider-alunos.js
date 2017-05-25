93e44824ddf8f8487a0c7a0479add4d5687411c2
Joab Galdino <Joab Galdino>
2017-05-25 10:45:44 -0300

    tela de alunos

---
+++ scripts/meformei.web.slider-alunos.js
@@ -0,0 +1,72 @@
+var slideralunos = slideralunos || {};
+
+var carrosselDiv = $('#carrossel-alunos');
+var carrosselMolde = $("#carrossel-molde").html();
+var carrosselTemplate = Handlebars.compile(carrosselMolde);
+
+var detalhesDiv = $('#detalhes-aluno');
+var detalhesMolde = $("#aluno-molde").html();
+var detalhesTemplate = Handlebars.compile(detalhesMolde);
+  
+/*var owl;*/
+slideralunos.alunos = {};
+
+var alunoIndex = 0;
+
+slideralunos.init = function(alunos){
+  slideralunos.carregaralunos(alunos);
+}
+
+slideralunos.changeCurrentImage = function() {
+  var aluno = slideralunos.alunos[alunoIndex];
+  var alunoHtml    = detalhesTemplate(aluno);
+  detalhesDiv.html(alunoHtml);
+
+  /*var fotoAlunoUrl = aluno.foto;
+  $aluno = $('#aluno');
+  $aluno.css("background", "url('" + fotoAlunoUrl + "') center fixed");
+  $aluno.css("background-size", "cover");*/
+}
+
+slideralunos.nextItem = function() {
+  alunoIndex += 1;
+  if (alunoIndex == slideralunos.alunos.length) {
+    alunoIndex = 0;  
+  }
+  slideralunos.changeCurrentImage();
+}
+
+slideralunos.previousItem = function() {
+  alunoIndex -= 1;
+  if (alunoIndex < 0) {
+    alunoIndex = slideralunos.alunos.length - 1;
+  }
+  slideralunos.changeCurrentImage();
+}
+
+slideralunos.carregaralunos = function(alunos){
+  slideralunos.alunos = alunos;
+  var alunosHtml = carrosselTemplate({'alunos': alunos});
+  carrosselDiv.html(alunosHtml);
+  owl = $('.owl-carousel');
+  owl.owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive: {
+          600:{
+            items:4
+          },
+          900:{
+            items:6
+          },
+          1200:{
+            items:7 
+          }
+        }
+   });
+
+  slideralunos.changeCurrentImage();
+}
+
