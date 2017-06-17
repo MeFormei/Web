ed4191e5f5761447c0bd665176b284f6e010daf2
Roberto Dantas <7robertodantas@gmail.com>
2017-06-17 14:03:14 -0300

    Teste unit backend em deploy/test.sh

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo 'Executing Unit Test in Backend'
+docker pull 7robertodantas/backend:latest
+docker run --rm --name backend -it 7robertodantas/backend:latest npm test
 echo 'Testing ssh connection with server'
 status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $user@$host echo ok 2>&1)
 if [[ $status == ok ]] ; then
