8dc6c748bc785c61aa7944781f6f2447dd1bd1b9
Roberto Dantas <7robertodantas@gmail.com>
2017-06-17 12:49:13 -0300

    Exit Success em deploy/test.sh

--- deploy/test.sh
+++ deploy/test.sh
@@ -2,7 +2,7 @@
 echo 'Testing ssh connection with server'
 status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $user@$host echo ok 2>&1)
 if [[ $status == ok ]] ; then
-  echo auth ok
+  echo auth_ok && exit 0
 elif [[ $status == "Permission denied"* ]] ; then
   echo no_auth && exit 126
 else
